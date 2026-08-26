be308c2742ad02eff9d15488e4806e9754b84381
Lem0n3de8 <lem0n3de8@gmail.com>
2026-08-26 17:33:22 +0200

    Feat: new function to add playlists to UI

--- content/playlist-ui.js
+++ content/playlist-ui.js
@@ -13,4 +13,29 @@ async function showPlaylistUI() {
     const html = await response.text();
 
     document.body.insertAdjacentHTML("beforeend", html);
+
+    console.log("Calling playlists");
+    displayPlaylists();
+}
+
+async function displayPlaylists(){
+    const playlists = await getPlaylists();
+    console.log("Playlists are: ", playlists);
+
+    const button = document.getElementById("create-playlist")
+
+    for (const playlist of playlists){
+        console.log("NEW PLAYLIST:", playlist);
+        const div = document.createElement("div");
+        const h3 = document.createElement("h3");
+
+        div.id = playlist.id;
+        div.classList.add("playlist-div");
+        
+        h3.textContent = playlist.name;
+        
+        div.appendChild(h3);
+        button.before(div);
+
+    }
 }
