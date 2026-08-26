b7808c3f227fc6e33dcb6f78f20bc14debb6f0d7
Lem0n3de8 <lem0n3de8@gmail.com>
2026-08-26 09:13:25 +0200

    Simple playlist ui

---
+++ content/youtube-ui.js
@@ -0,0 +1,16 @@
+async function showPlaylistUI() {
+    const UI_id = "playlist-overlay"
+    const existingUI = document.getElementById(UI_id);
+
+    if (existingUI){
+        existingUI.remove();
+        return;
+    }
+
+    const url = browser.runtime.getURL("content/youtube-ui.html");
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    document.body.insertAdjacentHTML("beforeend", html);
+}
